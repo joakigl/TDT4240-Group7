6f8220016c9cfc6be80216e501dce4bd25e472a2
Andreas <tb.tapion@gmail.com>
2016-04-22 17:25:01 +0200

    removed current src folder

--- src/com/mygdx/game/Handlers/TouchHandler.java
+++
@@ -1,7 +0,0 @@
-package com.mygdx.game.Handlers;
-
-/**
- * Created by Andreas on 17.04.2016.
- */
-public class TouchHandler extends {
-}
--- src/com/mygdx/game/PowerUps/BallSpeedDown.java
+++
@@ -1,7 +0,0 @@
-package com.mygdx.game.PowerUps;
-
-/**
- * Created by Andreas on 16.04.2016.
- */
-public class BallSpeedDown extends PowerUp {
-}
